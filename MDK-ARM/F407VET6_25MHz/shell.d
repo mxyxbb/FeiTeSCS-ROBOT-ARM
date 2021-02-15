d634f8f273344812def20b0e58ca472d57a25de0
mxy <mxy815@163.com>
2021-02-15 18:14:48 +0800

    first upload

---
+++ MDK-ARM/F407VET6_25MHz/shell.d
@@ -0,0 +1,7 @@
+f407vet6_25mhz\shell.o: ..\Core\Src\letter_shell\src\shell.c
+f407vet6_25mhz\shell.o: ..\Core\Src\letter_shell\src\shell.h
+f407vet6_25mhz\shell.o: ..\Core\Src\letter_shell\src\shell_cfg.h
+f407vet6_25mhz\shell.o: D:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+f407vet6_25mhz\shell.o: D:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+f407vet6_25mhz\shell.o: D:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdarg.h
+f407vet6_25mhz\shell.o: ..\Core\Src\letter_shell\src\shell_ext.h
---
+++ MDK-ARM/F407VET6_25MHz/shell_companion.d
@@ -0,0 +1,3 @@
+f407vet6_25mhz\shell_companion.o: ..\Core\Src\letter_shell\src\shell_companion.c
+f407vet6_25mhz\shell_companion.o: ..\Core\Src\letter_shell\src\shell.h
+f407vet6_25mhz\shell_companion.o: ..\Core\Src\letter_shell\src\shell_cfg.h
